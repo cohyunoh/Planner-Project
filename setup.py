d2db9483437ab918a4110f426f0e8e3e66d916e1
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-11-20 20:05:05 -0500

    moded setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 """
     This is the setup script for the Flask app.
 """
-from distutils.core import setup
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(name="Planner Flask App",
       version="1.0",
+      author="Jun Tao Lei, Connor Oh, Leia Park",
       description="A Planner Built With Flask",
-      author="Jun Tao Lei, Connor Oh",
-      packages=["flaskr"],
-      include_package_data=True,
+      long_description=long_description,
+      long_description_content_type="text/markdown"
       url="https://github.com/cohyunoh/Planner-Project.git",
-      install_requires=["flask"])
+      packages=find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.8")
