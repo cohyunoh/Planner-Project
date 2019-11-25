a5faa6729c61421df023dc14eb25317f5112f907
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-11-25 12:28:50 -0500

    fixed config setting

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE
 IF NOT EXISTS users
 (
   unqiueId INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
+  name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   homeAddress TEXT,
   workAddress TEXT,
