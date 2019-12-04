603e67c5af3803a622e4433230d4ab26373da5f1
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-12-03 23:36:44 -0500

    final

--- flaskr/reddit/reddit_view.py
+++ flaskr/reddit/reddit_view.py
@@ -3,14 +3,18 @@
 """
 from .reddit_funcs import get_posts
 from ..utl import login_check, get_from_user
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request
 
 REDDIT = Blueprint("REDDIT", __name__)
 
 
 @REDDIT.route("/news")
+@REDDIT.route("/reddit/sort")
 @login_check
 def news():
+    preference = "top"
+    if "sortBy" in request.args:
+        preference = request.args["sortBy"].lower()
     news = get_posts(get_from_user(session["user"]["email"], "newsPreference"),
-                     "top", 15)
+                     preference, 15)
     return render_template("news.html", news=news)
